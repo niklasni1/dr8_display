89960c792ea2008dd91370978e76c2348bc8b249
Niklas Nisbeth <niklas@nisbeth.dk>
2015-02-06 18:47:07 +0100

    stuff

---
+++ commands.h
@@ -0,0 +1,11 @@
+#define ALL_OFF         0b11110000
+#define ALL_ON          0b11110011
+#define NORMAL          0b11110001
+#define AUTO_INCR_ON    0b11110101
+#define AUTO_INCR_OFF   0b11110100 
+#define FULL_LENGTH     0b00000111
+#define HALF_LENGTH     0b00000001
+
+#define WRITE_TO_RAM    0b11111111 //2 lsb = n/c
+
+#define CURSOR_ON       0b00010000
